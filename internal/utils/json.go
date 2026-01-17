1fa2e3806d9b8fc1a6926b1283c68e9bc9a7f656
hona <116949627+hona518@users.noreply.github.com>
2026-01-18 03:59:57 +0800

    Create json.go

---
+++ internal/utils/json.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+    "encoding/json"
+    "net/http"
+)
+
+func JSON(w http.ResponseWriter, status int, v interface{}) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(status)
+    _ = json.NewEncoder(w).Encode(v)
+}
+
+func Error(w http.ResponseWriter, status int, msg string) {
+    JSON(w, status, map[string]interface{}{
+        "error": msg,
+    })
+}
